8c72991688c29ac6ba1769ab7374e9006f754972
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-06-22 17:23:26 -0500

    Agregando atributos

---
+++ src/mx/com/alurahotel/modelo/Huesped.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.alurahotel.modelo;
+
+/**
+ * Clase para el modelado del objeto Huesped.
+ * @author Eduardo Reyes Hernández
+ */
+public class Huesped {
+    
+    private Integer idHuesped;
+    private String nombre;
+    private String apellido;
+    private String fechaNacimiento;
+    private String nacionalidad;
+    private String telefono;
+    private Integer idReserva;
+    
+}
