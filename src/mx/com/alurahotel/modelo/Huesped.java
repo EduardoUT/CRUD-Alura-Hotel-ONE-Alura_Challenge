2f4fe36e1e92c433f1b887a87bf61ecb6323396a
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-06-22 18:18:50 -0500

    Agregando getters y setters

--- src/mx/com/alurahotel/modelo/Huesped.java
+++ src/mx/com/alurahotel/modelo/Huesped.java
@@ -4,6 +4,8 @@
  */
 package mx.com.alurahotel.modelo;
 
+import java.util.Date;
+
 /**
  * Clase para el modelado del objeto Huesped.
  * @author Eduardo Reyes Hernández
@@ -18,4 +20,101 @@ public class Huesped {
     private String telefono;
     private Integer idReserva;
     
+    /**
+     * @return the idHuesped
+     */
+    public Integer getIdHuesped() {
+        return idHuesped;
+    }
+
+    /**
+     * @param idHuesped the idHuesped to set
+     */
+    public void setIdHuesped(Integer idHuesped) {
+        this.idHuesped = idHuesped;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the fechaNacimiento
+     */
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    /**
+     * @param fechaNacimiento the fechaNacimiento to set
+     */
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    /**
+     * @return the nacionalidad
+     */
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    /**
+     * @param nacionalidad the nacionalidad to set
+     */
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the idReserva
+     */
+    public Integer getIdReserva() {
+        return idReserva;
+    }
+
+    /**
+     * @param idReserva the idReserva to set
+     */
+    public void setIdReserva(Integer idReserva) {
+        this.idReserva = idReserva;
+    }
 }
