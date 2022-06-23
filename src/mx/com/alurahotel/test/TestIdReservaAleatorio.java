a6d74782623ab5bdc1253dbe1aa924b6f24561d1
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-06-23 17:40:39 -0500

    Agregando clase de prueba para Ids con UUID

---
+++ src/mx/com/alurahotel/test/TestIdReservaAleatorio.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.alurahotel.test;
+
+import mx.com.alurahotel.modelo.Reserva;
+
+/**
+ *
+ * @author Eduardo Reyes Hernández
+ */
+public class TestIdReservaAleatorio {
+    public static void main(String[] args) {
+        Reserva r = new Reserva();
+        System.out.println(r);
+    }
+}
