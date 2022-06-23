1a42cb52199fb4a6f81d943b84cc905caeae5089
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-06-23 18:17:53 -0500

    Creando Controlador para Reserva

---
+++ src/mx/com/alurahotel/controller/ReservaController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.alurahotel.controller;
+
+import java.util.List;
+import mx.com.alurahotel.dao.ReservaDAO;
+import mx.com.alurahotel.factory.ConnectionFactory;
+import mx.com.alurahotel.modelo.Reserva;
+
+/**
+ * Clase que alberga todas las acciones de operación entre la capa View y la
+ * capa DAO.
+ *
+ * @author Eduardo Reyes Hernández
+ */
+public class ReservaController {
+
+    private final ReservaDAO reservaDAO;
+
+    /**
+     * Creando conexion para operaciones con MySQL con la tabla de reservas.
+     */
+    public ReservaController() {
+        this.reservaDAO = new ReservaDAO(new ConnectionFactory().realizarConexion());
+    }
+
+    /**
+     * Obteniendo el listado del DAO de huespedes.
+     *
+     * @return - List de reservas existentes.
+     */
+    public List<Reserva> listar() {
+        return reservaDAO.listar();
+    }
+}
